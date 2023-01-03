90361406dda58cac0d09a5935647b1d18ac8b803
Koustav Ghosh <koustav210@gmail.com>
2023-01-03 21:21:44 +0530

    code to find string length

---
+++ StringLength.java
@@ -0,0 +1,7 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Koustav";
+        System.out.println(str.length());
+        System.out.println(str.substring(0, 2));
+    }
+}
